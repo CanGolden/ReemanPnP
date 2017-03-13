9f4e0bae8094f8cd24fca7cd504f2f6b36d6cca3
ZJcan <zzuzhangjincan@163.com>
2017-03-13 17:43:40 +0800

    test update1

---
+++ app/src/main/java/com/jcan/reemanpnp/BaseActivity.java
@@ -0,0 +1,26 @@
+package com.jcan.reemanpnp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by ZJcan on 2017-03-13.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void onButton(View v) {
+
+    }
+}
--- app/src/main/java/com/jcan/reemanpnp/MainActivity.java
+++ app/src/main/java/com/jcan/reemanpnp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.jcan.reemanpnp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.jcan.reemanpnp.forcamera.CameraAty;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onButton(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                startActivity(new Intent(this, CameraAty.class));
+                break;
+            default:
+                break;
+        }
     }
 }
